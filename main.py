5c1fa89cc4b389222fdb9c3721a7beaca219a0cf
Neel <nprosofficial@gmail.com>
2019-08-19 09:28:02 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,2 +1,61 @@
-print("Hello World")  # This will display hello world
+# Solution to Question 1
+print("Hello Everyone !!!") 
 
+# Solution to Question 2
+print("""Hello
+World""")  
+
+# Solution to Question 3
+print("""Hello
+      World""") 
+
+# Solution to Question 4
+x = "Hello"
+print(x) 
+
+# Solution to Question 5
+print("""Brian's mother: He's not the messiah.
+  He's a very naughty boy !""") 
+
+# Solution to Question 6.
+print("""Twinkle, Twinkle, little star,
+
+                      How I wonder What you are !
+
+                      Up above the world so high,
+
+                      Like a diamond in the sky.
+
+            Twinkle, Twinkle, little star,
+
+                        How I wonder What you are.""") 
+
+#Solution to Question 7
+a = "Neel"
+b = "Adwani"
+c = a + b
+print(c) 
+
+# Solution to Question 8.
+fn = "Neel "
+ln = "Adwani"
+nn = "( neeltron ) "
+print(fn+nn+ln) 
+
+# Solution to Question 9
+name = "Neel"
+print(name * 3) 
+
+# Solution to Question 10
+name2 = "Neel       "
+print(name2 * 3) 
+
+# Solution to Question 11
+fname = "Neel"
+lname = "Adwani"
+add = "Campus Hostel"
+dob = "Jan 19th, 2019"
+prog = "AI and ML"
+sem = 1
+sap = 500075823
+print("Name: ", fname, lname, "\n\tSAP ID: ", sap, "Date of Birth: ", dob, "\nAddress: ", add, "Programme: ", prog) 
